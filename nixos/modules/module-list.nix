5bc2eb58a33b5cb2e3ea4cbce08130582704bb66
Kerstin Humm <kerstin@erictapen.name>
2023-11-09 14:17:45 +0100

    fix mistakes through a faulty rebase

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1454,7 +1454,6 @@
   ./services/web-apps/tt-rss.nix
   ./services/web-apps/vikunja.nix
   ./services/web-apps/weblate.nix
-  ./services/web-apps/wiki-js.nix
   ./services/web-apps/whitebophir.nix
   ./services/web-apps/wiki-js.nix
   ./services/web-apps/windmill.nix

f21c1e7eb135b5f50491bb8267a7de6c2d3bf6be
Kerstin Humm <kerstin@erictapen.name>
2024-07-02 13:13:56 +0200

    weblate: start packing natively without poetry2nix

--- pkgs/servers/web-apps/weblate/default.nix
+++ pkgs/servers/web-apps/weblate/default.nix
@@ -1,86 +1,111 @@
 { lib
+, python3
 , fetchFromGitHub
-, poetry2nix
-, gettext
-, isocodes
-, xorg
 , wrapGAppsNoGuiHook
 , gobject-introspection
 , pango
 , harfbuzz
+, borgbackup
+, nixosTests
 }:
 
-(poetry2nix.mkPoetryApplication {
+python3.pkgs.buildPythonApplication rec {
+  pname = "weblate";
+  version = "5.6.2";
+
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "WeblateOrg";
-    repo = "weblate";
-    rev = "weblate-4.14.1";
-    sha256 = "sha256-p+nwNEgHmMxQLhG+n9/6nywE71vO8kZgq77JwHYqPUs=";
+    repo = pname;
+    rev = "weblate-${version}";
+    sha256 = "sha256-t/hnigsKjdWCkUd8acNWhYVFmZ7oGn74+12347MkFgM=";
   };
 
-  pyproject = ./pyproject.toml;
-  poetrylock = ./poetry.lock;
+  build-system = with python3.pkgs; [ setuptools ];
 
-  patches = [
-    # The default timeout for the celery check is much too short upstream, so
-    # we increase it. Otherwise the integration test fails. I guess this is due
-    # to the fact that we test the setup very early into the initialization of
-    # the server, so the load might be higher compared to production setups?
-    ./longer-celery-wait-time.patch
+  nativeBuildInputs = [
+    wrapGAppsNoGuiHook
+    gobject-introspection
   ];
-
-  overrides = poetry2nix.overrides.withDefaults (
-    self: super: {
-      aeidon = super.aeidon.overridePythonAttrs (old: {
-        src = fetchFromGitHub {
-          owner = "otsaloma";
-          repo = "gaupol";
-          rev = "1.11";
-          sha256 = "sha256-iF2ScQFYxYM2o18Cfy6U6JAZxIiZSsGrWEb4yjyECwc=";
-        };
-        nativeBuildInputs = [ gettext ];
-        buildInputs = [ isocodes ];
-        installPhase = ''
-          ${self.python.interpreter} setup.py --without-gaupol install --prefix=$out
-        '';
-      });
-      click-didyoumean = super.click-didyoumean.overridePythonAttrs (old: {
-        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ self.poetry ];
-      });
-      pyparsing = super.pyparsing.overridePythonAttrs (old: {
-        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ self.flit-core ];
-      });
-      # ua-parser = super.ua-parser.overridePythonAttrs (old: {
-      #   nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ self.pyyaml ];
-      #   postPatch = ''
-      #     substituteInPlace setup.py \
-      #       --replace "pyyaml ~= 5.4.0" "pyyaml~=6.0"
-      #   '';
-      # });
-      jarowinkler = super.jarowinkler.overridePythonAttrs (old: {
-        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ self.scikit-build ];
-      });
-      rapidfuzz = super.rapidfuzz.overridePythonAttrs (old: {
-        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ self.scikit-build ];
-      });
-      jsonschema = super.jsonschema.overridePythonAttrs (old: {
-        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ self.hatch-fancy-pypi-readme ];
-      });
-    }
-  );
 
   buildInputs = [
     pango
     harfbuzz
   ];
 
+  dependencies = with python3.pkgs; [
+    aeidon
+    # ahocorasick-rs
+    borgbackup
+    celery # [redis]
+    certifi
+    charset-normalizer
+    # crispy-bootstrap3
+    cryptography
+    cssselect
+    cython
+    diff-match-patch
+    django-appconf
+    django-celery-beat
+    django-compressor
+    django-cors-headers
+    django-crispy-forms
+    django-filter
+    django-redis
+    django
+    djangorestframework
+    filelock
+    fluent-syntax
+    GitPython
+    hiredis
+    html2text
+    iniparse
+    jsonschema
+    lxml
+    misaka
+    mistletoe
+    nh3
+    openpyxl
+    packaging
+    phply
+    pillow
+    pycairo
+    pygments
+    pygobject3
+    # pyicumessageformat
+    pyparsing
+    python-dateutil
+    (python-redis-lock.override { withDjango = true; })
+    rapidfuzz
+    redis
+    requests
+    ruamel-yaml
+    sentry-sdk
+    # siphashc
+    social-auth-app-django
+    social-auth-core
+    tesserocr
+    translate-toolkit
+    # translation-finder
+    user-agents
+    # weblate-language-data
+    # weblate-schemas
+  ] ++ django.optional-dependencies.argon2;
+
   meta = with lib; {
     description = "Web based translation tool with tight version control integration";
-    homepage = https://weblate.org/;
+    homepage = "https://weblate.org/";
     license = licenses.gpl3Plus;
     maintainers = with maintainers; [ erictapen ];
   };
 
-}).dependencyEnv
+  passthru = {
+    tests = { inherit (nixosTests) weblate; };
+    optional-dependcencies = {
+      postgres = with python3.pkgs; [ psycopg ];
+    };
+  };
+
+}
 
